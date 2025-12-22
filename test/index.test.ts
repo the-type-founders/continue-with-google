ffb44d25e13f5fd489b18c2d3060f0abd943ed3d
Ivan Ukhov <ivan.ukhov@gmail.com>
2025-12-22 12:18:02 +0100

    Add a test

---
+++ test/index.test.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import assert from 'node:assert';
+import PuppeteerExtra from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { test } from 'vitest';
+
+import { authenticate } from '../src/index.js';
+
+test('authenticate', { timeout: 5 * 60 * 1000 }, async () => {
+  PuppeteerExtra.use(StealthPlugin());
+  const browser = await PuppeteerExtra.launch({
+    headless: false,
+    args: ['--no-sandbox'],
+    slowMo: parseInt(process.env.PUPPETEER_SLOW_MOTION || '100'),
+  });
+  try {
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.goto('https://mail.google.com');
+    await assert.doesNotReject(
+      authenticate(
+        page,
+        process.env.GOOGLE_USER_EMAIL!,
+        process.env.GOOGLE_USER_PASSWORD!,
+        process.env.GOOGLE_USER_SECRET!,
+        '[aria-label="Search mail"]'
+      )
+    );
+  } finally {
+    await browser.close();
+  }
+});
